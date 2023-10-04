13596bb82299ca203561db7922ff9a65b1f086e3
yatziry <a3520110146@alumno.uttehuacan.edu.mx>
2023-10-03 20:34:21 -0600

    creacion del back

---
+++ index.ts
@@ -0,0 +1,15 @@
+const express = require('express');
+//traspilar
+//EcmaScript ES modules
+//import express from 'express';
+//CommonJS const express = require('express');
+const app = express();
+
+const example = function () {
+    console.log('estoy a la escucha');
+}
+
+app.listen(
+    9000,
+    example
+)
