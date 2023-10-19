deb15f4532ce9ce68010c05c73ad6c3eea8bdfc9
YatziryP <79027350+YatziryP@users.noreply.github.com>
2023-10-18 20:57:52 -0600

    base de datos conexion

--- src/network/user/index.ts
+++ src/network/user/index.ts
@@ -1,5 +1,12 @@
-function userGet(){
-    console.log("userGet");
+import express, {Request, Response} from 'express';
+
+const router = express.Router();
+
+function getUserByEmail(req: Request, res: Response){
+    const { email } = req.params;
+    res.send('User With email ${email}');
 }
 
-export default userGet;
+router.get('/', getUserByEmail);
+
+export default router;
